3ad5a22f4249fafa6df3af08bcdf6e9f21adba51
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-12-18 11:23:21 +0100

    404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+import { getCurrentMonthName } from './src/utils';
+
+
+const month = getCurrentMonthName();
+const year = new Date().getFullYear();
 
 export default defineConfig({
+  redirects: {
+    '/mejores/peliculas': `/mejores/peliculas/${year}/${month}`,
+  },
   output: "server",
   adapter: node({
     mode: "standalone"
