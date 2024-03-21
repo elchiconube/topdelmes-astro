0531fe83ba709fe121819d811c7072def176dd8d
Oscar Bustos <oscarbustosgarrido@gmail.com>
2024-03-21 16:40:47 +0100

    add cron

--- cron.js
+++ cron.js
@@ -1,5 +1,5 @@
-const cron = require('node-cron');
-const { exec } = require('child_process');
+import cron from 'node-cron';
+import { exec } from 'child_process';
 
 cron.schedule('0 0 * * *', () => {
   console.log('Ejecutando npm run build:sitemaps una vez al día');
