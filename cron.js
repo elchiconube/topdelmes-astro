08ced31bafb57813ae2049d78efd4de422e0650b
Oscar Bustos <oscarbustosgarrido@gmail.com>
2024-03-21 16:38:23 +0100

    add cron

---
+++ cron.js
@@ -0,0 +1,19 @@
+const cron = require('node-cron');
+const { exec } = require('child_process');
+
+cron.schedule('0 0 * * *', () => {
+  console.log('Ejecutando npm run build:sitemaps una vez al día');
+  exec('npm run build:sitemaps', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al ejecutar el comando: ${error}`);
+      return;
+    }
+    console.log(`Salida: ${stdout}`);
+    if (stderr) {
+      console.error(`Error: ${stderr}`);
+    }
+  });
+}, {
+  scheduled: true,
+  timezone: "Europe/Madrid" // Zona horaria para Madrid, España
+});
